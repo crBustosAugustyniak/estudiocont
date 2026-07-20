72de7c44c9a639ec9058bb4b60b6d09c7b099cbc
crBustosAugustyniak <bustosyamila8@gmail.com>
2026-07-20 12:34:41 -0300

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+const supabaseUrl = "https://bpmasqikmimzviugmrvp.supabase.co"
+const supabaseKey = "sb_publishable_9m0mpqG0RBtWngvJWbRSVw_GBy3U61m"
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
